095792994dcc549d4edd37459055c612008f1664
abh1nav <abhinavsharma00410@gmail.com>
2024-04-24 23:53:19 +0530

    Dropdown categories done

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,5 +13,17 @@ export class LoginComponent {
       loginObj:any ={
         userName: '',
         password:''
+      }
+
+      constructor(private router: Router){
+
+      }
+
+      onLogin(){
+        if(this.loginObj.userName=="admin" && this.loginObj.password == "abhi"){
+          this.router.navigateByUrl('/products')
+        } else{
+          alert('Wrong Credentials')
+        }
       }
 }
--- src/app/pages/website/category-products/category-products.component.ts
+++ src/app/pages/website/category-products/category-products.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './category-products.component.css'
 })
 export class CategoryProductsComponent {
-
+    
 }
